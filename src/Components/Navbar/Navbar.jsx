65465a2e09ab747fe3eff99c56b012305b1c5de4
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-11-06 16:22:55 +0500

    navbar finished

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import "./Navbar.css";
+import logo from "../../images/logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faGlobe,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <nav>
+      <div className="nav__logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="nav__searchBar">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <input type="text"></input>
+      </div>
+      <ul className="nav__menu">
+        <li>
+          <a href="/#">Asosiy</a>
+        </li>
+        <li>
+          <a href="/#">Mahsulotlar</a>
+        </li>
+        <li>
+          <a href="/#">Bog'lanish</a>
+        </li>
+      </ul>
+      <ul className="nav__icons">
+        <li>
+          <a href="/#">
+            <FontAwesomeIcon icon={faCartShopping} />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <FontAwesomeIcon icon={faHeart} />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <FontAwesomeIcon icon={faGlobe} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
