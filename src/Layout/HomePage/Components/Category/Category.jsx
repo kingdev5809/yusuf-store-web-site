ed8577edc9e2be3c0a4f96e3dd8d0294e0974115
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-11-07 15:30:24 +0500

    Navigation added

--- src/Layout/HomePage/Components/Category/Category.jsx
+++ src/Layout/HomePage/Components/Category/Category.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./Category.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import { FreeMode, Pagination } from "swiper/modules";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 import CategoryItem from "./CategoryItem";
 
 function Category() {
@@ -15,14 +15,19 @@ function Category() {
         <Swiper
           slidesPerView={8}
           spaceBetween={30}
-          // freeMode={true}
           pagination={{
             clickable: true,
           }}
+          navigation={true}
           // loop={true}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
-        >
+          modules={[Pagination, Navigation]}
+          className="mySwiper">
+          <SwiperSlide>
+            <CategoryItem />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CategoryItem />
+          </SwiperSlide>
           <SwiperSlide>
             <CategoryItem />
           </SwiperSlide>
